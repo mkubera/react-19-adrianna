e288b9d1cd6d0f69e46c8ecea139b1589be52e07
mkubera <rainteller@tutanota.de>
2021-03-23 20:26:06 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./Title";
+import NavBtn from "./Components/Nav/NavBtn";
 
 function App() {
+  const title = "My First App";
+  const navBtns = [
+    { name: "Home", colour: "blue" },
+    { name: "Products", colour: "red" },
+    { name: "Contact", colour: "orange" },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
+        <Title title={title} />
+
+        <nav>
+          {navBtns.map((btn) => {
+            return <NavBtn name={btn.name} colour={btn.colour} />;
+          })}
+        </nav>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -13,8 +30,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
---
+++ src/Components/Nav/NavBtn.js
@@ -0,0 +1,9 @@
+function NavBtn(props) {
+  return (
+    <div>
+      <p style={{ color: props.colour }}>{props.name}</p>
+    </div>
+  );
+}
+
+export default NavBtn;
